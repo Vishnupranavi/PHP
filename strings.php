406c2aef0b19735328fb379db9e06fb6c259e3c1
Vishnupranavi <83103461+Vishnupranavi@users.noreply.github.com>
2022-07-20 19:13:53 +0530

    strings recall

--- strings.php
+++ strings.php
@@ -1,64 +1,90 @@
-<?php
-$str = addcslashes("Hello Vishnu","V");
-echo($str);
-echo "\n";
-$str1 = addslashes('Hello "welcome" to php');
-echo($str1);
-echo "\n";
-
-$str2=bin2hex("Vishnu");
-echo($str2);
-$str3="Vishnu pranavi";
-echo "\n";
-echo (chop($str3,"pranavi"));
-echo "\n";
-echo chr(55);
-echo chr(055);
-echo chr(0x52);
-$str4="Aspire family";
-echo "\n";
-echo chunk_split($str4,1,"*");
-$str5="Vishnu pranavi";
-echo "\n";
-echo count_chars($str5,3);
-echo "\n";
-$str = crc32("Hello World!");
-printf("%u\n",$str);
-
-$str = "Hello world. aspire family.";
-print_r (explode(" ",$str));
-echo hebrev("Hello");
-echo "\n";
-echo hex2bin("48656c6c6f20576f726c6421");
-echo "\n";
-$str6="thiis@#qwert";
-echo  htmlspecialchars_decode($str6);
-$arr=array('welcome','to', 'aspire', 'systems');
-echo implode("",$arr);
-echo "\n";
-echo lcfirst("Hai,welcome");
-echo "\n";
-echo levenshtein("Hello World","ello World");
-echo str_repeat("weasp",15);
-echo str_rot13("rotate");
-echo str_shuffle("Hello World");
-echo "\n";
-echo str_word_count("Hello world!");
-echo "\n";
-echo strcasecmp("Hello","HELLO");
-echo strcmp("Hello world!","Hello world!");
-echo "\n";
-echo strip_tags("Hello <b><i>world!</i></b>","<b>");
-echo "\n";
-
-echo strlen("Hello Aspire");
-$str7= "An example of a long word is: Supercalifragulistic";
-echo wordwrap($str7,15,"<br>\n");
-
-
-
-
-
-
-
-?> 
+<?php  
+    //chop
+    $str = "Hello Aspire!";  
+    echo "Original string :".$str."<br>";  
+    echo "Output string: ".chop($str,"Aspire!");  
+   //repeat
+    echo "output:".str_repeat("w",3);
+    //ireplace
+    $arr = array("Coimbatore","trichy","madurai","salem");  
+    echo "Original string: array('Coimbatore','trichy','madurai','salem')";  
+    echo "<br>";  
+    print_r(str_ireplace("trichy","bangalore",$arr,$i)); // This function is case-insensitive  
+    echo "Replacements: $i";  
+    //pad
+     $str = "Welcome Aspire";  
+    echo "Original string:".$str;  
+    echo "<br>";  
+    echo "Output:".str_pad($str,"28"," the lamp team",STR_PAD_RIGHT);;  
+    //replace
+     $string = "Hai team!";  
+    $search = 'Hai';  
+    $replace = 'Hello';  
+    echo '<b>'."Original String:".'</br></b>';  
+    echo $string.'</br>';  
+    $newstr = str_replace($search, $replace, $string, $count);  
+    echo '<b>'."Replaced string:".'</br></b>';  
+    echo $newstr.'</br>';  
+    echo 'Replacement Count ='.$count;  
+    //split
+    $str = "Hello team";
+    echo(str_split($str));
+    //suffle
+    $str = "Hello team";
+    echo str_shuffle($str);
+    //word_count
+    $str="Hello Team Welcome";  
+    echo "Input string:".$str;  
+    echo "<br>";  
+    echo "Output: ".str_word_count($str);  
+    echo(str_word_count($str,1));
+    //strcoll
+    $str1 = "Hello Team";
+    $str2 = "Hello Team";
+    $str3 = "Hi everyone";
+    echo strcoll($str1,$str3);
+    echo strcoll($str1,$str2);
+    //strlen
+    $str = "hai";
+    echo strlen($str);
+    //lower,upper
+    $str = "HI ASPIRIANS";
+    echo strtolower($str);
+    $str1 = "hi aspirians";
+    echo strtoupper($str1);
+    //strrev
+    $str = "Hello php";
+    echo strrev($str);
+    //strtr
+    $strng1="Hi tbam";  
+    $from= "ba";  
+    $to="ea";  
+    echo strtr($strng1, $from, $to); 
+    //substr
+    $str = "hello world";
+    echo substr($str,3);
+    //substr_compare
+    echo substr_compare("Hello Aspire","Aspire",0);
+    echo substr_compare("Hello Aspire","Lamp team",0);
+    echo substr_compare("Hello Aspire","Aspire",6);
+    //substr_count
+    $strin="Life gives good life lessons on everyday life of everyone"; 
+    $sub_str="life";  
+    echo substr_count($strin,$sub_str);  
+    //trim
+    $str = "Hello PHP!";  
+    echo "My string is: ".$str ."<br>";  
+    echo "output:".trim($str,"HeP!");  
+    //ucfirst
+    $str = "i am going to office";
+    echo ucfirst($str);
+    //ucwords
+    $str = "hai hello lamp team";
+    $str1 = "   hi|welcome|home";
+    echo ucwords($str);
+    echo ucwords($str1,"|");
+    //wordwrap
+    $str = "An example of a long word is: HappyHollyToAll";
+    echo wordwrap($str,15,"<br>\n");
+    
+    ?>  
