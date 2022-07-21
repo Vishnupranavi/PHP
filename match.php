d0a81dff6c3bf1df356f1c4e3de627db6f7ba1cc
Vishnupranavi <83103461+Vishnupranavi@users.noreply.github.com>
2022-07-21 19:29:08 +0530

    Execution operators

--- match.php
+++ match.php
@@ -1,9 +1,7 @@
 <?php
-$food = 'cake';
-$return_value = match ($food) {
-    'apple' => 'This food is an apple',
-    'bar' => 'This food is a bar',
-    'cake' => 'This food is a cake',
-};
-echo "$return_value";
+//execution operator
+$output = `ls -al`;
+echo "$output";
 ?>
+
+
